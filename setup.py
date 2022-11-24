59a5e41fe8960ce1271c253f7732ad844634dfab
Juraj Trappl <juro.trappl@gmail.com>
2022-11-24 15:20:51 +0100

    removed setup.py code

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='oarepo-upload-cli',
-    version='0.0.1',
-    install_requires=[
-        'argparse',
-        'pyenv',
-        'requests',
-        'importlib-metadata; python_version == "3.9"',
-    ],
-    entry_points={
-        'console_scripts': [
-            'upload = upload:main'
-        ]
-    }
-)
+setup()
