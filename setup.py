4a09bbb8725af306eeb48b7625ec807adc7be9d7
Juraj Trappl <juro.trappl@gmail.com>
2022-11-24 09:07:07 +0100

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='oarepo-upload-cli',
+    version='0.0.1',
+    install_requires=[
+        'argparse',
+        'pyenv',
+        'requests',
+        'importlib-metadata; python_version == "3.9"',
+    ]
+)
