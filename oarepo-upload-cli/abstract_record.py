7a359b7e36692166a96fa8b798b0626f66bf0c82
Juraj Trappl <juro.trappl@gmail.com>
2022-11-24 12:50:07 +0100

    added abstract record interface

---
+++ oarepo-upload-cli/abstract_record.py
@@ -0,0 +1,15 @@
+class AbstractRecordMeta(type):
+    """
+    Abstract record metaclass that is used for record creation.
+    """
+    def __instancecheck__(cls, instance):
+        return cls.__subclasscheck__(type(instance))
+    
+    def __subclasscheck__(cls, subclass):
+        return hasattr(subclass, 'get_metadata') and callable(subclass.get_metadata)
+    
+class AbstractRecordInterface(metaclass=AbstractRecordMeta):
+    """
+    Interface for concrete record.
+    """
+    pass
