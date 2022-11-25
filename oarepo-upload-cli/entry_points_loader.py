0247a1cdf2fb5dca19536996b47f75ebf2498cd0
Juraj Trappl <juro.trappl@gmail.com>
2022-11-25 12:43:37 +0100

    enhanced entry points with new conditions and importlib

--- oarepo-upload-cli/entry_points_loader.py
+++ oarepo-upload-cli/entry_points_loader.py
@@ -1,8 +1,8 @@
-import pkg_resources
+from importlib.metadata import entry_points
 from typing import Any, NoReturn
 
-from abstract_record import AbstractRecordInterface
-from abstract_record_source import AbstractRecordSourceInterface
+from abstract_record import AbstractRecord
+from abstract_record_source import AbstractRecordSource
 
 class EntryPointNotFoundException(Exception):
     """
@@ -11,8 +11,8 @@ class EntryPointNotFoundException(Exception):
     pass
 
 class EntryPointsLoader():
-    def load_abstract_record_source(self, source_name_arg: str = None) -> AbstractRecordSourceInterface | NoReturn:
-        group, name = 'dependencies', 'abstract_record_source'
+    def load_abstract_record_source(self, source_name_arg: str = None) -> AbstractRecordSource | NoReturn:
+        group, name = 'dependencies', 'oarepo-upload-source'
 
         abstract_record_source = self.__load(group, name, source_name_arg)
 
@@ -25,10 +25,10 @@ class EntryPointsLoader():
         return abstract_record_source.load()
 
     
-    def load_abstract_record(self, record_name_args: str = None) -> AbstractRecordInterface | NoReturn:
-        group, name = 'dependencies', 'abstract_record'
+    def load_abstract_record(self, group: str, name: str, record_arg_name: str = None) -> AbstractRecord | NoReturn:
+        group, name = 'dependencies', 'oarepo-upload-record'
 
-        abstract_record = self.__load(group, name, record_name_args)
+        abstract_record = self.__load(group, name, record_arg_name)
 
         if not abstract_record:
             raise EntryPointNotFoundException('Abstract record')
@@ -39,12 +39,20 @@ class EntryPointsLoader():
         return abstract_record.load()
 
     def __load(self, group: str, name: str, arg_name: str = None) -> Any | NoReturn:
-        eps = list(pkg_resources.iter_entry_points(group, name))
+        eps = entry_points().select(group=group, name=name)
 
         if not eps:
             raise ValueError('Entry point not provided')
         
-        if len(eps) > 1 and not arg_name:
+        if len(eps) == 1 and not arg_name:
+            # user wants the same entry point as it is defined
+            return eps[0]
+        
+        if len(eps) == 1 and arg_name and eps[0].name != arg_name:
+            # user wants an entry point that is not loaded
+            raise EntryPointNotFoundException(f'Entry point {arg_name} was not found.')
+
+        if not arg_name:
             raise ValueError('Multiple entry points present, can not choose one')
     
         for ep in eps:
