99a1875e90a6600458b364b4c5ee50449c165cd0
Juraj Trappl <juro.trappl@gmail.com>
2022-12-01 15:14:50 +0100

    small fixes

--- oarepo_upload_cli/abstract_record_source.py
+++ oarepo_upload_cli/abstract_record_source.py
@@ -9,14 +9,14 @@ class AbstractRecordSource(metaclass=abc.ABCMeta):
     Interface for concrete record source that generates records.
     """
     @abc.abstractmethod
-    def get_records(self, modified_after: datetime = None, modified_before: datetime = None) -> Iterable[AbstractRecord]:
+    def get_records(self, modified_after: datetime=None, modified_before: datetime=None) -> Iterable[AbstractRecord]:
         """
         Generator returning records of type AbstractRecord limited by given timestamps.
         If no timestamps are provided, returns all records.
         """
 
     @abc.abstractmethod
-    def get_records_count(self, modified_after: datetime = None, modified_before: datetime = None) -> int:
+    def get_records_count(self, modified_after: datetime=None, modified_before: datetime=None) -> int:
         """
         Approximate size of the collection of records returned.
         """
--- oarepo_upload_cli/entry_points_loader.py
+++ oarepo_upload_cli/entry_points_loader.py
@@ -1,12 +1,15 @@
 import importlib_metadata
-from typing import Any, NoReturn
+from typing import Any
 
 from abstract_record import AbstractRecord
 from abstract_record_source import AbstractRecordSource
 from exceptions import EntryPointNotFoundException
 
 class EntryPointsLoader():
-    def load_abstract_record_source(self, source_name_arg: str = None) -> AbstractRecordSource | NoReturn:
+    def load_abstract_record_source(self, source_name_arg: str=None) -> AbstractRecordSource:
+        """
+        """
+
         group, name = 'oarepo_upload_cli.dependencies', 'oarepo_upload_test_source'
 
         ep_record_source = self.__load(group, name, source_name_arg)
@@ -17,7 +20,10 @@ class EntryPointsLoader():
         return ep_record_source.load()
 
     
-    def load_abstract_record(self, record_arg_name: str = None) -> AbstractRecord | NoReturn:
+    def load_abstract_record(self, record_arg_name: str=None) -> AbstractRecord:
+        """
+        """
+        
         group, name = 'oarepo_upload_cli.dependencies', 'oarepo_upload_test_record'
 
         ep_record = self.__load(group, name, record_arg_name)
@@ -27,9 +33,8 @@ class EntryPointsLoader():
 
         return ep_record.load()
 
-    def __load(self, group: str, name: str, arg_name: str = None) -> Any | None:
+    def __load(self, group: str, name: str, arg_name: str=None) -> Any | None:
         """
-        todo
         """
 
         eps = importlib_metadata.entry_points().select(group=group, name=name)
@@ -38,11 +43,11 @@ class EntryPointsLoader():
             raise ValueError('Entry point not provided')
         
         if len(eps) == 1 and not arg_name:
-            # user wants the same entry point as it is defined
+            # user requested the same entry point as it is defined
             return eps[0]
         
         if len(eps) == 1 and arg_name and eps[0].name != arg_name:
-            # user wants an entry point that is not loaded
+            # user requested an entry point that is not loaded
             raise EntryPointNotFoundException(f'Entry point {arg_name} was not found.')
 
         if not arg_name:
