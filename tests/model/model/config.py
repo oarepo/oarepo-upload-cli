bbc035385fee8054b68a784cbf358310ab36eb58
Juraj Trappl <juro.trappl@gmail.com>
2023-06-02 11:40:49 +0200

    deleted not used model of tests

--- tests/model/model/config.py
+++
@@ -1,9 +0,0 @@
-from model.resources.config import ModelResourceConfig
-from model.resources.resource import ModelResource
-from model.services.config import ModelServiceConfig
-from model.services.service import ModelService
-
-MODEL_RESOURCE_CONFIG = ModelResourceConfig
-MODEL_RESOURCE_CLASS = ModelResource
-MODEL_SERVICE_CONFIG = ModelServiceConfig
-MODEL_SERVICE_CLASS = ModelService
