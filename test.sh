b886714f91809255a3ad4edc3319acfacfc159d2
Juraj Trappl <juro.trappl@gmail.com>
2023-06-02 09:08:13 +0200

    fixed test script

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -e
+
 source '.env'
 
-pip install -e tests/model[tests]
+if [ -d .venv_test ]; then rm -rf .venv_test; fi
+python3 -m venv .venv_test
+source .venv_test/bin/activate
+pip install -U pip setuptools wheel
+pip install -e uct_theses
 
 # TODO: direnv
 
@@ -9,8 +15,14 @@ export INVENIO_RECORDS_REFRESOLVER_CLS="invenio_records.resolver.InvenioRefResol
 export INVENIO_RECORDS_REFRESOLVER_STORE="invenio_jsonschemas.proxies.current_refresolver_store"
 export INVENIO_RATELIMIT_AUTHENTICATED_USER="200 per second"
 
+invenio index destroy --yes-i-know --force
 invenio index init --force
+
+if [ -f .venv_test/var/instance/invenio.db ]; then rm -f .venv_test/var/instance/invenio.db; fi
 invenio db create
+
+invenio oarepo cf init
+invenio oarepo fixtures load
 
 invenio run --cert test.crt --key test.key 2>&1 > tmp.error.log &
 invenio_pid=$!
