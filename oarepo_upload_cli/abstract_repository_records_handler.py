6826d889c5d3d3e06fd538f301c1525c310f80bb
Juraj Trappl <juro.trappl@gmail.com>
2023-06-02 11:49:52 +0200

    removed method call params

--- oarepo_upload_cli/abstract_repository_records_handler.py
+++ oarepo_upload_cli/abstract_repository_records_handler.py
@@ -116,7 +116,7 @@ class AbstractRepositoryRecordsHandler(ABC):
         
         # POST the file metadata (a key).
         post_files_url = f'{self._collection_url}{record_files_link}/files'
-        post_request_data = file.metadata()
+        post_request_data = file.metadata
         
         post_response = self._send_request('post', url=post_files_url, headers=self._headers, json=post_request_data, verify=False, auth=self._auth)
         
