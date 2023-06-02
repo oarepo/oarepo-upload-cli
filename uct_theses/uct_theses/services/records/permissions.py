1dc8ab6ff65411590243f754a649c84d5a57c197
Juraj Trappl <juro.trappl@gmail.com>
2023-06-02 13:06:25 +0200

    changed uct permissions

--- uct_theses/uct_theses/services/records/permissions.py
+++ uct_theses/uct_theses/services/records/permissions.py
@@ -1,5 +1,5 @@
 from invenio_records_permissions import RecordPermissionPolicy
-from invenio_records_permissions.generators import AnyUser, SystemProcess
+from invenio_records_permissions.generators import AnyUser, SystemProcess, AuthenticatedUser
 from invenio_records_resources.services.files.generators import AnyUserIfFileIsLocal
 
 
@@ -8,15 +8,15 @@ class UctThesesPermissionPolicy(RecordPermissionPolicy):
 
     can_search = [SystemProcess(), AnyUser()]
     can_read = [SystemProcess(), AnyUser()]
-    can_create = [SystemProcess(), AnyUser()]
-    can_update = [SystemProcess(), AnyUser()]
-    can_delete = [SystemProcess(), AnyUser()]
-    can_manage = [SystemProcess(), AnyUser()]
+    can_create = [SystemProcess(), AuthenticatedUser()]
+    can_update = [SystemProcess(), AuthenticatedUser()]
+    can_delete = [SystemProcess(), AuthenticatedUser()]
+    can_manage = [SystemProcess(), AuthenticatedUser()]
 
-    can_create_files = [AnyUser(), SystemProcess()]
-    can_set_content_files = [AnyUserIfFileIsLocal(), SystemProcess(), AnyUser()]
+    can_create_files = [AuthenticatedUser(), SystemProcess()]
+    can_set_content_files = [AnyUserIfFileIsLocal(), SystemProcess(), AuthenticatedUser()]
     can_get_content_files = [AnyUserIfFileIsLocal(), SystemProcess(), AnyUser()]
-    can_commit_files = [AnyUserIfFileIsLocal(), SystemProcess(), AnyUser()]
+    can_commit_files = [AnyUserIfFileIsLocal(), SystemProcess(), AuthenticatedUser()]
     can_read_files = [AnyUser(), SystemProcess()]
-    can_update_files = [AnyUser(), SystemProcess()]
-    can_delete_files = [AnyUser(), SystemProcess()]
+    can_update_files = [AuthenticatedUser(), SystemProcess()]
+    can_delete_files = [AuthenticatedUser(), SystemProcess()]
