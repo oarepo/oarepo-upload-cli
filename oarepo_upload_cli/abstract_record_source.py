ebcf61f60c84b01600e0ff3f0be21586e49ffe25
Juraj Trappl <juraj.trappl@proton.me>
2023-06-06 12:37:29 +0200

    added config to source

--- oarepo_upload_cli/abstract_record_source.py
+++ oarepo_upload_cli/abstract_record_source.py
@@ -3,11 +3,15 @@ from datetime import datetime
 from typing import Iterable
 
 from oarepo_upload_cli.abstract_record import AbstractRecord
+from oarepo_upload_cli.config import Config
 
 class AbstractRecordSource(ABC):
     """
     Describes a source that is used to generate records.
     """
+    
+    def __init__(self, config: Config) -> None:
+        self._config = config        
     
     @abstractmethod
     def get_records(self, modified_after: datetime=None, modified_before: datetime=None) -> Iterable[AbstractRecord]:
